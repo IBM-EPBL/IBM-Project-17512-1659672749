b7da0479270db4a06d3068961abfbf32e7c6f43a
sahasra120 <sahasrasrepreya@gmail.com>
2022-09-19 23:30:24 +0530

    update code

---
+++ Assignment/Team Leader- Sahasra/python Assignment - Module 3/list.py
@@ -0,0 +1,11 @@
+l=[7,17,18,1,2,333,151] 
+l.insert(4,115)
+print(l) 
+l.remove(30)
+print(l) 
+l.append(41)
+print(l) 
+print(sorted(l))
+print(l.pop()) 
+l.reverse()
+print(l)
---
+++ Assignment/Team Leader- Sahasra/python Assignment - Module 3/string.py
@@ -0,0 +1,6 @@
+a="hello"
+b="world"
+c=a+" "+b
+print(c) 
+print(c[::-1])
+print(c[1:3])
