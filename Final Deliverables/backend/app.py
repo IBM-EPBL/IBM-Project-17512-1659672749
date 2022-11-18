0a23aadfdb1bdcd0a15fae30de7c0afc57f00bdc
Sanjai-R <samsanjai777@gmail.com>
2022-11-18 18:24:17 +0530

    completed

--- Final Deliverables/backend/app.py
+++ Final Deliverables/backend/app.py
@@ -1,20 +1,11 @@
-from dotenv import dotenv_values
 from flask import Flask
 from flask_cors import CORS
 import ibm_db
 
-# Get the environment variables
-config = dotenv_values("backend/.env")
 
 # Connect to db
-try:
-    # conn = 'dd'
-    conn = ibm_db.pconnect(
-        f"DATABASE={config['DB2_DATABASE']};HOSTNAME={config['DB2_HOSTNAME']};PORT={config['DB2_PORT']};SECURITY=SSL; SSLServerCertificate=backend/DigiCertGlobalRootCA.crt;UID={config['DB2_USERNAME']};PWD={config['DB2_PASSWORD']}", '', '')
-    print("Connected to IBM_DB2 successfully!!")
-    print(conn)
-except:
-    print("Failed to connect to Database!")
+conn=ibm_db.connect("DATABASE=bludb;QUERYTIMEOUT=1;CONNECTTIMEOUT=10;HOSTNAME=55fbc997-9266-4331-afd3-888b05e734c0.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31929;SECURITY=SSL;SSLServerCertificate=./DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=jyg49227;PWD=TWWOggsuvcgpokMW",'','')
+print(conn)
 
 
 def create_app():
@@ -24,7 +15,7 @@ def create_app():
     CORS(app)
 
     # Set the secret key for flask
-    app.config['SECRET_KEY'] = config['APP_SECRET']
+    app.config['SECRET_KEY'] = 'test'
 
     # Import and register auth_router
     from .auth_router import auth
