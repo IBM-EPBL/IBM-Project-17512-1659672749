7eaef5ac8acd2f2660efa7b40b9be9ccc0f58af2
nirmal7699 <nirmalrajselvaraj7699@gmail.com>
2022-11-19 11:30:36 +0530

    updated app.py

--- Final Deliverables/backend/app.py
+++ Final Deliverables/backend/app.py
@@ -3,21 +3,21 @@ from flask_cors import CORS
 import ibm_db
 
 
-# Connect to db
+
 conn=ibm_db.connect("DATABASE=bludb;QUERYTIMEOUT=1;CONNECTTIMEOUT=10;HOSTNAME=55fbc997-9266-4331-afd3-888b05e734c0.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31929;SECURITY=SSL;SSLServerCertificate=./DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=jyg49227;PWD=TWWOggsuvcgpokMW",'','')
 print(conn)
 
 
 def create_app():
-    # Tell flask to use the build directory of react to serve static content
+    
     app = Flask(__name__, static_folder='../dist', static_url_path='/')
 
     CORS(app)
 
-    # Set the secret key for flask
+    
     app.config['SECRET_KEY'] = 'test'
 
-    # Import and register auth_router
+   
     from .auth_router import auth
     app.register_blueprint(auth, url_prefix='/api/auth')
 
