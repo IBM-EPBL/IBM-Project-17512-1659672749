2c939b1eff1a475d3f36218b8b212d77ce93a6c0
Sanjai-R <samsanjai777@gmail.com>
2022-11-19 23:04:41 +0530

    Finalized report and completed app

--- Final Deliverables/backend/sendEmail.py
+++ Final Deliverables/backend/sendEmail.py
@@ -7,7 +7,7 @@ def sendEmail(payload):
     from_email='737819ECR155@smartinternz.com',
     to_emails = payload["email"],
     subject='Verify Your Email Address',
-    html_content='<h3>Please Verify Your Email Address By Click The Link Below</h3><br /><a href="http://127.0.0.1:5000/verification/{}">Verify Now</a>'.format(payload["email"]))
+    html_content='<h3>Please Verify Your Email Address By Click The Link Below</h3><br />'.format(payload["email"]))
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
